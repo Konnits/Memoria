0dbb37c43635b3f7a749151266743e6103149f27
konnits <gerald.mendez@sansano.usm.cl>
2026-04-29 10:31:21 -0400

    Add DILATELoss to training module and export in __all__

--- src/ts_transformer/training/__init__.py
+++ src/ts_transformer/training/__init__.py
@@ -8,6 +8,7 @@ Incluye:
 - Bucle de entrenamiento de alto nivel (Trainer).
 """
 
+from .dilate_loss import DILATELoss
 from .losses import get_loss_fn
 from .metrics import compute_regression_metrics
 from .optimizers import OptimizerConfig, build_optimizer, build_scheduler
@@ -15,6 +16,7 @@ from .train_loop import TrainingConfig, Trainer, train_model
 
 __all__ = [
     "get_loss_fn",
+    "DILATELoss",
     "compute_regression_metrics",
     "OptimizerConfig",
     "build_optimizer",
